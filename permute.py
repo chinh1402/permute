47551c4e40885cb26fec3aabe360fa2e74393846
Chinh <ndchinh2003@gmail.com>
2024-06-26 10:55:44 +0700

    first commit

---
+++ permute.py
@@ -0,0 +1,46 @@
+def permute(k, arr):
+    n = len(arr)
+    permutation = ""
+    for i in range(0, n):
+        permutation = permutation + k[arr[i] - 1]
+    return permutation
+
+def straight_permutation():
+    k = "ABCDEFGH"
+    arr = [3, 1, 4, 7, 6, 2, 5, 8]
+    n = len(arr)
+    print("Straight Permutation:")
+    print("Before permutation:", k)
+    print("arr:", arr)
+    result = permute(k, arr)
+    print("After permutation:", result)
+    print("Input Length:", len(k), "Output Length:", len(result))
+    print()
+
+def extend_permutation():
+    k = "ABCDEFGH"
+    arr = [3, 1, 4, 7, 6, 2, 5, 8, 3, 1]
+    n = len(arr)
+    print("Extend Permutation:")
+    print("Before permutation:", k)
+    print("arr:", arr)
+    result = permute(k, arr)
+    print("After permutation:", result)
+    print("Input Length:", len(k), "Output Length:", len(result))
+    print()
+
+def compress_permutation():
+    k = "ABCDEFGH"
+    arr = [3, 1, 4, 7]
+    n = len(arr)
+    print("Compress Permutation:")
+    print("Before permutation:", k)
+    print("arr:", arr)
+    result = permute(k, arr)
+    print("After permutation:", result)
+    print("Input Length:", len(k), "Output Length:", len(result))
+    print()
+
+# straight_permutation()
+# extend_permutation()
+# compress_permutation()
